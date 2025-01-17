cba86e5edebe78fcf8bbd05952d12a0a0077e93b
Vladimir Bulatov <vladimir.bulatov@myoffice.team>
2025-01-17 13:22:38 +0300

    Update MPL v2.0 copyrights

--- dav4kmp/build.gradle.kts
+++ dav4kmp/build.gradle.kts
@@ -1,3 +1,11 @@
+/*
+ *
+ *  * This Source Code Form is subject to the terms of the Mozilla Public
+ *  * License, v. 2.0. If a copy of the MPL was not distributed with this
+ *  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
 import com.vanniktech.maven.publish.SonatypeHost
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
